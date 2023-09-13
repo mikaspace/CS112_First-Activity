ddecacfee4e05435fe45d8c212bfe776c91040c3
myks <144907016+mikaspace@users.noreply.github.com>
2023-09-13 04:33:13 -0700

    Add files via upload

---
+++ baclayo.py
@@ -0,0 +1,11 @@
+print('Myka Angelie G. Baclayo')
+
+print('BS Computer Science')
+
+print('CS1E')
+
+print('20 years old')
+
+print('November 01, 2002')
+
+print('Kolambog Lapasan, Cagayan De Oro City, Misamis Oriental')
